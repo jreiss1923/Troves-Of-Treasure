854ba495e43562fbaf4bfe0af1bf2ec60dd96a1c
jreiss1923 <joshua.reiss2@gmail.com>
2019-12-12 02:16:08 -0500

    Created functions, learned card id query

--- trovesoftreasure.py
+++ trovesoftreasure.py
@@ -5,14 +5,26 @@ headers = {
     "Authorization": "bearer JijuljIEFOaaV2CIU7cpXc_uODdf0euxYhOGolYFlY8Ivqiz4nbnGcHZxwSbvav10ElZx7pQfvO4DnZ0qD1UnL3xc5l2uQP3xUBGp4U_Av35LhZlUH3S2sqVUN5XSxU5g6EhjAHyZPT1WqsXD_bet7vD48la6cLB0E-verohCqtERHVjXFQtFRut_gXmJrmESYP9vq4WH5sTPWbactvev7AvV2g7SF6hGix-yhXxR2SbgVyAWmTtsrlKM52mEB3SZaGVvdaEAbKGV-AtXmjpjymAsgvlYErcraSWiqOcwHQOj6rGKCprIRIs_pIw8-4cw9C1_A"
 }
 
-#headers = {
-#    'app': 'application/x-www-form-urlencoded',
-#}
+def search_card_prices(cardString):
+    params = {
+        "productName": cardString
+    }
+    product_prices = []
+    response = requests.get("http://api.tcgplayer.com/v1.32.0/catalog/products", headers=headers, params=params).json()
+    for i in range(len(response["results"])):
+        product_prices.append(response["results"][i]["productId"])
 
-data = 'grant_type=client_credentials&client_id=' + PUBLIC_KEY + '&client_secret=' + PRIVATE_KEY
-bearer_token = "JijuljIEFOaaV2CIU7cpXc_uODdf0euxYhOGolYFlY8Ivqiz4nbnGcHZxwSbvav10ElZx7pQfvO4DnZ0qD1UnL3xc5l2uQP3xUBGp4U_Av35LhZlUH3S2sqVUN5XSxU5g6EhjAHyZPT1WqsXD_bet7vD48la6cLB0E-verohCqtERHVjXFQtFRut_gXmJrmESYP9vq4WH5sTPWbactvev7AvV2g7SF6hGix-yhXxR2SbgVyAWmTtsrlKM52mEB3SZaGVvdaEAbKGV-AtXmjpjymAsgvlYErcraSWiqOcwHQOj6rGKCprIRIs_pIw8-4cw9C1_A"
+def price_of_card(cardId):
+    print("hi")
 
-#response = requests.post('https://api.tcgplayer.com/token', headers=headers, data=data)
-response = requests.get("http://api.tcgplayer.com/v1.32.0/catalog/categories", headers=headers)
+#def bearer_token(PUBLIC_KEY, PRIVATE_KEY):
+    # bearer_headers = {
+    #    'app': 'application/x-www-form-urlencoded',
+    # }
 
-print(response.text)
+    # data = 'grant_type=client_credentials&client_id=' + PUBLIC_KEY + '&client_secret=' + PRIVATE_KEY
+
+    # response = requests.post('https://api.tcgplayer.com/token', headers=bearer_headers, data=data)
+    # bearer_token = response[whatever i have to get]
+
+search_card_prices("noble hierarch")
